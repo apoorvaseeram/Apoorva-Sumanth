2f2c3eb10595654accbda78ed1bc163e25e7b8b6
Sumanth Kudapa <sumanthkudapa@Sumanths-MacBook-Pro.local>
2020-11-12 21:03:47 -0500

    Header content and css

--- apoorva/src/Components/Header.js
+++ apoorva/src/Components/Header.js
@@ -1,14 +1,29 @@
 import React from "react";
 
 const Header = () => {
+  
   return (
-    <div>
-      <h2>
-        {" "}
-        <b> Welcome to our React Application</b>
-      </h2>
-      <hr></hr>
-    </div>
+          
+          <nav>
+            <table>
+              <tr>
+                <th class="thclass"><a href="google.com">Home</a></th>
+                <th class="thclass"><a href="google.com">About</a></th>
+                <th class="thclass"><a href="google.com">Collections</a></th>
+                <th class="thclass"><a href="google.com">Sale</a></th>
+                <th class="thclass"><a href="google.com">Gallery</a></th>
+                <th class="thclass"><a href="google.com">Sale</a></th>
+                <th class="thclass"><a href="google.com">Contact</a></th>
+              </tr>
+            </table>
+            <div>
+              <h2>
+                {" "}
+                   <b> Welcome to our React Application</b>
+              </h2>
+              <hr></hr>
+            </div>
+          </nav>
   );
 };
 
